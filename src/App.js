95c90c461c9bcf80117585529e5b132943b4d210
peter9378 <peter9378@naver.com>
2021-03-21 13:26:09 +0000

    this is my best effort

--- src/App.js
+++ src/App.js
@@ -5,18 +5,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className="Artist-name">
+          HASUN KAREN KANG
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+	<p className="Artist-detail">
+	  A PAINTER AND ALSO A PRODUCT DESIGNER.
+	  LOVING ART & DESIGN
+	</p>
       </header>
     </div>
   );
